8e957272bb755e2f9693ec6d58ea2f6ee35ff19c
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-29 23:44:55 +0530

    [NIJAM] Add:Employee_Payroll_Service Using ADO.NET.

---
+++ EmployeePayrollService_ADO.NET/EmployeeRepo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService_ADO.NET
+{
+    public class EmployeeRepo
+    {
+        public static string connectString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectString);
+
+        public void CheckConnection()
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    connection.Open();
+                    Console.WriteLine("Databased_Connected....");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Database_Not_connected!!!!");
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
---
+++ EmployeePayrollService_ADO.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeePayrollService_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**ADO.NET**");
+            EmployeeRepo emp = new EmployeeRepo();
+            EmployeeModel empModel = new EmployeeModel();
+            emp.CheckConnection();
+        }
+    }
+}
